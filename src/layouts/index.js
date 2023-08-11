075512a6b1c192a865c1036b53ede11bd5a2df13
Sean Preston <sean.preston@turquoise.health>
2023-08-11 15:03:21 -0600

    use an arrow

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -73,7 +73,7 @@ class TemplateWrapper extends Component {
     return (
       <div>
         <Helmet
-          title={`${site.siteMetadata.title} &#129046; ${site.siteMetadata.name}`}
+          title={`${site.siteMetadata.title} → ${site.siteMetadata.name}`}
         />
         <Header
           name={site.siteMetadata.name}
